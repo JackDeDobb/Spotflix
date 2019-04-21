2645de96cf17e93cebad045de40d395915659444
Jackson DeDobbealere <jackdedobb@me.com>
2019-04-20 20:31:00 -0500

    scarped and added all metadata stuff to aws s3 bucket

--- movieScraper.py
+++ movieScraper.py
@@ -25,7 +25,9 @@ def scrape_dir_page(dir_url, browser):
     print ('-'*20,'Scraping movies','-'*20)
     movie_links = []
     movie_titles = []
-    movie_base_url = 'https://www.imsdb.com/scripts'
+    meta_links = []
+    movie_base_url = 'https://www.imsdb.com/scripts/' 
+    meta_base_url = 'https://www.imsdb.com/Movie Scripts/'
     
     #execute js on webpage to load faculty listings on webpage and get ready to parse the loaded HTML 
     soup = get_js_soup(dir_url, browser)
@@ -36,13 +38,15 @@ def scrape_dir_page(dir_url, browser):
         child = link.find_all('p')
         for script in child:
             # lots of quirky formatting issues, kind of mcguyvered this part lel
-            movie_titles.append(script.find('a')['href'][15:-12])
-            script_url = script.find('a')['href'][14:-12].replace(' ', '-') + '.html'
+            title = script.find('a')['href'][15:-12]
+            movie_titles.append(title)
+            script_url = title.replace(' ', '-') + '.html'
+            meta_links.append((meta_base_url + title + ' Script.html').replace(' ', '%20'))
             movie_links.append(movie_base_url + script_url)
                 
     print ('-'*20,'Found {} movie urls'.format(len(movie_links)),'-'*20)
     print(movie_titles)
-    return movie_links, movie_titles
+    return movie_links, movie_titles, meta_links
 
 def make_dict(script):
     formatAndSplit = re.sub(r'[^A-Za-z]', ' ', script).lower().split()
@@ -62,6 +66,37 @@ def get_movie_script(link):
     except:
         return None
 
+def getMetaTuple(link, browser):
+    soup = get_js_soup(link, browser)
+    info = soup.find('table', class_='script-details')
+    rating = None
+    releaseDate = None
+    moviePic = None
+    genreList = []
+
+    try:
+        ratingSoup = str(info.find('b', text='Average user rating').next_sibling.next_sibling.next_sibling.next_sibling).strip()
+        rating = ratingSoup[ratingSoup.index('(') + 1:ratingSoup.index(' ')]
+    except:
+        pass
+
+    try:
+        releaseDateSoup = str(info.find('b', text='Script Date').next_sibling)
+        releaseDate = releaseDateSoup[releaseDateSoup.index(':') + 1:].strip()
+    except:
+        pass
+    
+    moviePic = info.find('img')['src']
+
+    genre = info.find('b', text='Genres').next_sibling.next_sibling.next_sibling
+    while str(genre).startswith("<a href=\"/genre/"):
+        soupString = str(genre)
+        genreList.append(soupString[16:soupString.index('\"', 16)])
+        genre = genre.next_sibling.next_sibling.next_sibling
+
+
+    return ((rating, releaseDate, moviePic, genreList))
+
 
 if __name__ == "__main__":
     options = Options()
@@ -73,26 +108,28 @@ if __name__ == "__main__":
     alpha_d = ['0'] + list(string.ascii_uppercase)
     total_movie_links = []
     total_titles = []
+    total_metas = []
     for l in alpha_d:
         print("Page: ", base_url + l)
-        links, titles = scrape_dir_page(base_url + l, browser)
+        links, titles, metas = scrape_dir_page(base_url + l, browser)
         print("Links: ", links)
         if links != None:
             total_movie_links.extend(links)
             total_titles.extend(titles)
-    print("Total Number of Scripts : " + str(len(total_movie_links)) )
+            total_metas.extend(metas)
+    print("Total Number of Scripts : " + str(len(total_movie_links)))
 
 
     script_dicts = []
     title_list = []
-    for idx, link in enumerate(total_movie_links):
+    meta_list = []
+    for idx, link in enumerate(total_movie_links[:40]):
         script_dict = get_movie_script(link)
         if script_dict != None:
             print("got script dict ", idx)
             title_list.append((total_titles[idx], sum(script_dict.values())))
             script_dicts.append(script_dict)
-    #print(script_dicts[0])
-    print(title_list)
+            meta_list.append(getMetaTuple(total_metas[idx], browser))
 
 
     #code to create df dictionary
@@ -118,11 +155,10 @@ if __name__ == "__main__":
     obj = s3.Object('movie-dicts','titleList.json')
     obj.put(Body=json.dumps(title_list))
 
+    obj = s3.Object('movie-dicts','metaList.json')
+    obj.put(Body=json.dumps(meta_list))
+
     obj = s3.Object('movie-dicts','dfDict.json')
     obj.put(Body=json.dumps(df_dict))
 
-    #json.dump_s3(script_dicts, "listOfMovies")
     print("finished")
-    #s3_connection = boto.connect_s3()
-    #s3 = boto3.resource('s3')
-    #s3.create_bucket(Bucket='listOfMovieDicts')
